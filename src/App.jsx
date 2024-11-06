04a879fd34c7c942447fc32b96616a775d3fa92d
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-06 07:52:34 +0100

    Formularz do dodawania nowego projektu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProject from "./components/NewProject.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
+    <main className="h-screen my-8 flex gap-8 ">
+        <ProjectsSidebar />
+        <NewProject />
     </main>
   );
 }
---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+export default function Input({ label, isTextArea, ...props }) {
+    return (
+        <p>
+            <label>{label}</label>
+            {isTextArea ? <textarea {...props} /> : <input {...props} />}
+        </p>
+
+    );
+}
