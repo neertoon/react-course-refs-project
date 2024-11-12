7960aecdd19b99a7f8531c805cb5cca4633e9ea9
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-12 08:11:14 +0100

    Stylowanie modala i akcja dla przycisku Cancel

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import Input from "./Input.jsx";
 import {useRef} from 'react';
 import Modal from "./Modal.jsx";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -28,14 +28,14 @@ export default function NewProject({onAdd}) {
     return (
         <>
             <Modal ref={modal} buttonCaption="Close">
-                <h2>Invalid Input</h2>
-                <p>Oops... looks like you forgot to enter wartości</p>
-                <p>Uzupełnmij wszystkie wartości w inputach</p>
+                <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+                <p className="text-stone-600 mb-4 ">Oops... looks like you forgot to enter wartości</p>
+                <p className="text-stone-600 mb-4 ">Uzupełnmij wszystkie wartości w inputach</p>
             </Modal>
             <div className="w-[35rem]  mt-16">
                 <menu className="flex items-center justify-end gap-4 my-4 ">
                     <li>
-                        <button className="text-stone-800 hove:text-stone-950">Cancel</button>
+                        <button onClick={onCancel} className="text-stone-800 hove:text-stone-950">Cancel</button>
                     </li>
                     <li>
                         <button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 ">Save</button>
