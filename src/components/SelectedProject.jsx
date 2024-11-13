17326f655fec1beaa47de1b5af5f3904a76ae9fe
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-11-13 08:12:49 +0100

    Usuwanie projektu

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,5 +1,5 @@
 
-export default function SelectedProject({ project }) {
+export default function SelectedProject({ project, onDelete }) {
     const date = new Date(project.dueDate).toLocaleDateString('en-US', {
         year: 'numeric',
         month: 'short',
@@ -11,7 +11,7 @@ export default function SelectedProject({ project }) {
             <header className="pb-4 mb-4 border-b-2 border-stone-300 ">
                 <div className="flex items-center justify-between ">
                     <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-                    <button className="text-stone-600 hover:text-stone-950">DELETE</button>
+                    <button onClick={onDelete} className="text-stone-600 hover:text-stone-950">DELETE</button>
                 </div>
                 <p className="mb-4 text-stone-400">{date}</p>
                 <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
